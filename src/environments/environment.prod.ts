ccd97e4cbb6e63eeed26a187eb88576ad00afdd2
Hlulani Baloyi <hlullyr@gmail.com>
2023-10-01 05:47:33 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDi-16-Jeuy36jY3Do04H61tCjMYmBR570",
+    authDomain: "edu-advisor-68d1c.firebaseapp.com",
+    projectId: "edu-advisor-68d1c",
+    storageBucket: "edu-advisor-68d1c.appspot.com",
+    messagingSenderId: "141207164261",
+    appId: "1:141207164261:web:8829a613d80faeb96b3d8c",
+    measurementId: "G-LBE6GBKSL3"
+  }
 };
